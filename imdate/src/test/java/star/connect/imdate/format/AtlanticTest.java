ab555d75d947c8ca19ef44996f2788f98efb2d45
maprohu <maprohu@gmail.com>
2016-02-15 17:16:57 +0000

    fixing repos

--- imdate/src/test/java/star/connect/imdate/format/AtlanticTest.java
+++ imdate/src/test/java/star/connect/imdate/format/AtlanticTest.java
@@ -60,4 +60,46 @@ public class AtlanticTest {
 
 
     }
+
+    @Test
+    public void test2() throws JAXBException {
+        AtlanticFormat format = new AtlanticFormat();
+
+        FormatResult formatResult = new FormatResult() {
+
+            @Override
+            public void error(String message, Throwable e) {
+                System.out.println(message);
+                System.out.println(e);
+//                throw Throwables.propagate(e);
+            }
+
+            @Override
+            public void error(long lineNumber, String field, String message, Throwable e) {
+                System.out.println(lineNumber);
+                System.out.println(field);
+                System.out.println(message);
+                System.out.println(e);
+//                throw Throwables.propagate(e);
+            }
+        };
+
+        Atlantic atlantic = format.parseCSV(AtlanticTest.class.getResourceAsStream("/atlantic_valid.csv"), formatResult);
+
+        JAXBContext jc = JAXBContext.newInstance(Atlantic.class, ProjectOvrRootType.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        m.marshal(atlantic, new File("target/atlantic2.xml"));
+
+
+        format.validate(atlantic, formatResult);
+
+        JAXBElement<ProjectOvrRootType> cdf = format.convertToCDF(atlantic);
+
+        m.marshal(cdf, new File("target/atlantic2.cdf.xml"));
+
+
+
+
+    }
 }
